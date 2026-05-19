a22092105b4ecc66fa69c651c4dc170bee168ec3
Luong Fu <lmp14589@gmail.com>
2026-05-19 13:20:13 +0700

    [DE190150] docs: Rearrange project into 'source' folder

--- lib/features/booking/domain/usecases/cancel_booking_usecase.dart
+++
@@ -1,9 +0,0 @@
-import '../repositories/booking_repository.dart';
-
-class CancelBookingUseCase {
-  final BookingRepository repository;
-
-  CancelBookingUseCase(this.repository);
-
-  Future<void> call(String bookingId) => repository.cancelBooking(bookingId);
-}
