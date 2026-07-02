c6defd7c70473793f513ccb1f4406d12e9cd07c5
KhanhHoa212 <khanhhoakt2k4@gmail.com>
2026-07-02 15:34:38 +0700

    feat: add GuideController and views for notifications and support pages

--- source/web/TripMate_Webapi/TripMate_Webapi/Controllers/GuideController.cs
+++ source/web/TripMate_Webapi/TripMate_Webapi/Controllers/GuideController.cs
@@ -303,6 +303,32 @@ namespace TripMate_Webapi.Controllers
 
             return View();
         }
+
+        // GET: /Guide/Notifications
+        public IActionResult Notifications()
+        {
+            var notifications = new List<dynamic>
+            {
+                new { Id = 1, Type = "booking_new", Title = "Booking mới từ Trần Thị Bảo Châu", Message = "Đặt 'Bình minh Mỹ Sơn' vào 15/06/2026. Phản hồi trong 24h", Time = "10 phút trước", IsRead = false },
+                new { Id = 2, Type = "payment", Title = "Thanh toán đã được giải ngân", Message = "850,000đ từ booking #BK-2024-008 đã về ví", Time = "1 giờ trước", IsRead = false },
+                new { Id = 3, Type = "review", Title = "Đánh giá mới 5 sao từ Nguyễn Văn An", Message = "\"Guide rất nhiệt tình, rất recommend!\"", Time = "Hôm qua 14:23", IsRead = true },
+                new { Id = 4, Type = "admin", Title = "Tài khoản của bạn đã được xác minh", Message = "Chào mừng bạn đến với TripMate Local Guide. Bắt đầu tạo tour ngay nhé!", Time = "2 ngày trước", IsRead = true }
+            };
+            ViewBag.Notifications = notifications;
+            return View();
+        }
+
+        // GET: /Guide/Support
+        public IActionResult Support()
+        {
+            var tickets = new List<dynamic>
+            {
+                new { Id = "TK-001", Title = "Khách hủy sát giờ", Status = "Pending", Date = "12/06/2026" },
+                new { Id = "TK-002", Title = "Lỗi không nhận được tiền giải ngân", Status = "Resolved", Date = "05/06/2026" }
+            };
+            ViewBag.Tickets = tickets;
+            return View();
+        }
     }
 
     // View Models
